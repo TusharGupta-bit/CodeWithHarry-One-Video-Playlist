1c2224fe33748b4a8a6f14266f5282228cfaaee2
Tushar <tushargupta1234@hotmail.com>
2020-10-19 22:08:20 +0530

    Updates

---
+++ precache-manifest.6c2879c66aa1fdfb6fa0308dc0d7e903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ef991af77b59290d3907897996ddd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757c7005ad408a379831",
+    "url": "/static/css/main.b07f77c0.chunk.css"
+  },
+  {
+    "revision": "74c9e3bfd522d708f9e2",
+    "url": "/static/js/2.6bbbd5f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bbbd5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757c7005ad408a379831",
+    "url": "/static/js/main.d646d3bc.chunk.js"
+  },
+  {
+    "revision": "cea0ac2b81d53d8e1754",
+    "url": "/static/js/runtime-main.7cfa012c.js"
+  }
+]);
